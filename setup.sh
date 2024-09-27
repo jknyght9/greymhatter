01512c50fe5ea03c5ed7f3661760f7f3158e6042
Jacob Stauffer <jstauffer@coherentcyber.com>
2024-09-26 22:04:09 -0500

    continuing user environment config

--- setup.sh
+++ setup.sh
@@ -41,7 +41,7 @@ ff02::2 ip6-allrouters
 EOF
 
 echo -e "${GREEN}[+] Installing required software${NC}"
-dnf install bat btop curl fish duf exa gnome-shell-extension-apps-menu git neovim openssl python3 python3-pip tmux util-linux-user wget -y
+dnf install bat btop curl fish duf exa gnome-shell-extension-apps-menu gnome-shell-extension-system-monitor git neovim openssl python3 python3-pip tmux util-linux-user wget -y
 
 echo -e "${GREEN}[+] Installing Hack Nerd Fonts${NC}"
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Hack.zip\
@@ -68,20 +68,29 @@ if id "$USERNAME" &>/dev/null; then
 else 
   useradd -m -G wheel $USERNAME -p "$ENC_PASSWORD" -s $(which fish)
 fi
+xhost +SI:localuser:hatter
 chsh -s $(which fish) $USERNAME
 cp $CURRENT_DIR/media/greymhatter-background.jpg /home/$USERNAME/Pictures/background.jpg
+cp $CURRENT_DIR/media/greymhatter-logo.png /var/lib/AccountService/icons/$USERNAME
 chown $USERNAME:$USERNAME /home/$USERNAME/Pictures/background.jpg
 
 echo -e "${GREEN}[+] Switching to ${USERNAME}${NC}"
 su - $USERNAME << EOF
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && chown -R 1000:1000 ~/.tmux
-dconf write /org/gnome/desktop/background/picture-uri "'file:///home/hatter/Pictures/background.jpg'"
-dconf write /org/gnome/desktop/background/picture-options "'zoom'"
-dconf write /org/gnome/desktop/wm/preferences/button-layout "':minimize,maximize,close'"
-dconf write /org/gnome/desktop/interface/monospace-font-name "'Hack Nerd Font 10'"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && chown -R 1000:1000 ~/.tmux:
+gsettings set org.gnome.desktop.background picture-uri 'file:///home/hatter/Pictures/background.jpg'
+gsettings set org.gnome.desktop.background picture-uri-dark 'file:///home/hatter/Pictures/background.jpg'
+gsettings set org.gnome.desktop.background picture-options 'zoom'
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,maximize,close'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Hack Nerd Font 10'
+gsettings set org.gnome.shell favorite-apps "['org.mozilla.firefox.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.TextEditor.desktop']"
 gnome-extensions enable apps-menu@gnome-shell-extensions.gcampax.github.com
+gnome-extensions enable blur-my-shell@aunetx
+gnome-extensions enable caffeine@patapon.info
 EOF
 echo -e "${GREEN}[+] Switching to $(whoami)${NC}"
+cp -r $CURRENT_DIR/conky/conkyrc /home/$USERNAME/.conkyrc
+chown -R $USERNAME:$USERNAME /home/$USERNAME/.conkyrc
 cp -r $CURRENT_DIR/config/* /home/$USERNAME/.config
 chown -R $USERNAME:$USERNAME /home/$USERNAME/.config
 
@@ -105,7 +114,7 @@ else
   echo "Yeti installation completed"
 fi
 
-read -p "Enter to continue"
+read -p "Enter to continue"joun
 
 echo -e "${GREEN}[+] Installing Timesketch${NC}"
 bash ./install-timesketch.sh "$CURRENT_DIR" "$USERNAME" "$PASSWORD"
