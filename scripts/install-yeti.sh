e53083b97efe40c84fb94c170a479741124a8ec6
Jacob Stauffer <jstauffer@coherentcyber.com>
2024-10-15 18:52:52 -0500

    fixing yeti variables

--- scripts/install-yeti.sh
+++ scripts/install-yeti.sh
@@ -17,7 +17,7 @@ git clone https://github.com/yeti-platform/yeti-docker
 cd /opt/yeti-docker/prod
 sed -i 's/- 80:80/- 8888:80/' docker-compose.yaml
 docker compose up -d
-API = $(docker compose -p yeti exec -it api /docker-entrypoint.sh create-user $USERNAME $PASSWORD --admin)
+API=$(docker compose -p yeti exec -it api /docker-entrypoint.sh create-user $USERNAME $PASSWORD --admin)
 if [[ "$API" == "*API key:*" ]]; then
   APIKEY = $($API | cut -d ":" -f 3 | xargs)
   sed -i s/YETI_API_ROOT = ''/YETI_API_ROOT = 'https://yeti-frontend/api/v2'/g /opt/timesketch/etc/timesketch/timesketch.conf
