c10f25901f72a1fbbcc053918ebb6c29f973c40d
Jacob Stauffer <jstauffer@coherentcyber.com>
2024-10-17 09:22:28 -0500

    adding more to the homepage and fixing yeti setup

--- scripts/install-yeti.sh
+++ scripts/install-yeti.sh
@@ -18,10 +18,10 @@ cd /opt/yeti-docker/prod
 sed -i 's/- 80:80/- 8888:80/' docker-compose.yaml
 docker compose up -d
 API=$(docker compose run --rm api create-user $USERNAME $PASSWORD --admin)
-if [[ "$API" == "*API key:*" ]]; then
+if [[ "$API" == *"API key:"* ]]; then
   APIKEY=$($API | cut -d ":" -f 3 | xargs)
-  sed -i s/YETI_API_ROOT = ''/YETI_API_ROOT = 'https://yeti-frontend/api/v2'/g /opt/timesketch/etc/timesketch/timesketch.conf
-  sed -i s/YETI_API_KEY = ''/YEI_API_ROOT = '$APIKEY'/g /opt/timesketch/etc/timesketch/timesketch.conf
+  sed -i s/YETI_API_ROOT = ''/YETI_API_ROOT = 'https://localhost:8000/api/v2'/g /opt/timesketch/etc/timesketch/timesketch.conf
+  sed -i s/YETI_API_KEY = ''/YEI_API_KEY = '$APIKEY'/g /opt/timesketch/etc/timesketch/timesketch.conf
   echo -e "Yeti API key set for Timesketch"
 else
   echo -e "Yeti API key not set in Timesketch"
