8ad62e3db7522fc3430cab6778bd359e8983c549
Jacob Stauffer <jstauffer@coherentcyber.com>
2024-10-24 09:20:44 -0500

    adding installation packages and updating readme

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+USERNAME="$1"
+
 echo -e "Installing Hayabusa"
 CWD=$(pwd)
 mkdir /opt/tools/
@@ -10,6 +12,7 @@ cd hayabusa
 mv hayabusa*-musl hayabusa
 chmod +x hayabusa
 ./hayabusa update-rules
+ln -s /opt/tools/hayabusa/hayabusa /home/$USERNAME/.local/bin/hayabusa
 cd ..
 rm hayabusa*.zip 
 
@@ -17,10 +20,15 @@ echo -e "Installing Malware Analysis Tools"
 cd /opt/tools
 wget $(wget -q -O - https://api.github.com/repos/mandiant/capa/releases/latest | jq -r '.assets[] | select(.name | contains ("linux.zip")) | .browser_download_url')
 wget $(wget -q -O - https://api.github.com/repos/mandiant/flare-floss/releases/latest | jq -r '.assets[] | select(.name | contains ("linux")) | .browser_download_url')
+wget $(wget -q -O - https://api.github.com/repos/VirusTotal/vt-cli/releases/latest | jq -r '.assets[] | select(.name | contains ("Linux64")) | .browser_download_url')
 rm capa*-py311.zip 
 unzip capa*.zip 
 unzip floss*.zip
-rm capa*.zip floss*.zip
+unzip Linux64.zip
+ln -s /opt/tools/capa /home/$USERNAME/.local/bin/capa
+ln -s /opt/tools/floss /home/$USERNAME/.local/bin/floss
+ln -s /opt/tools/vt /home/$USERNAME/.local/bin/vt
+rm capa*.zip floss*.zip Linux64.zip
 
 echo -e "Installing Sleuthkit"
 cd /opt/tools
@@ -36,5 +44,6 @@ make install
 fls -V
 dnf groupremove "Development Tools" -y
 dnf remove autoconf automake libtool maven -y
+cd ..
 rm -rf sleuthkit*
 cd "$CWD"
