6d638ff0a253908001b9b69c384edcb6eb0d9984
Jacob Stauffer <jstauffer@coherentcyber.com>
2024-10-24 20:13:41 -0500

    adding concurrent installation

--- scripts/install-volatility2.sh
+++ scripts/install-volatility2.sh
@@ -6,6 +6,7 @@ CWD=$(pwd)
 wget -O vol2.zip https://github.com/volatilityfoundation/volatility/releases/download/2.6.1/volatility_2.6_lin64_standalone.zip
 unzip vol2.zip
 mkdir -p /home/$USERNAME/.local/bin
+chown -R "$USERNAME:$USERNAME" /home/$USERNAME/.local
 mv ./volatility_2.6_lin64_standalone/volatility_2.6_lin64_standalone /home/$USERNAME/.local/bin/vol2
 rm -rf vol2*
 rm -rf volatilitiy*
