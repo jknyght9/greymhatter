0ce15f18386823f7d20f066194f0ba0f8e7377b0
Jacob Stauffer <jstauffer@coherentcyber.com>
2025-02-27 22:45:15 -0600

    fixing bugs

--- scripts/install-volatility3.sh
+++ scripts/install-volatility3.sh
@@ -30,7 +30,7 @@ cp -r symbols/symbols/windows/ /home/$USERNAME/.local/lib/python3.13/site-packag
 echo -e "Downloading malware sample and caching symbol tables, this will take a while"
 curl -O https://cc-public.s3.amazonaws.com/0zapftis.zip
 unzip -P infected 0zapftis.zip
-#vol -vvv -f 0zapftis.vmem windows.info
+vol -vvv -f 0zapftis.vmem windows.info
 rm -rf 0zapftis* linux.zip mac.zip symbols/ symbols.sha256 volatility3/ windows.zip yara-python/
 EOF
 echo -e "Switching to back to root"
